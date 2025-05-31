4fa2b3c5f86d97d250847c52701db1c3acbcbb75
parkashmi09 <78411265+parkashmi09@users.noreply.github.com>
2025-06-01 03:04:19 +0530

    push

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || 'https://api.cfztechnologies.store',
+  API_URL: import.meta.env.VITE_API_URL || 'https://api.cfztechnologies.store/api',
 }; 

995e431ecc62f12e0dba3b34dbe6d80f3b7c191b
parkashmi09 <78411265+parkashmi09@users.noreply.github.com>
2025-07-09 17:56:25 +0530

    push

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = 'https://api.cfztechnologies.store/api';
 
 // Get all contacts with pagination
 export const getContacts = async (page = 1, limit = 10) => {
